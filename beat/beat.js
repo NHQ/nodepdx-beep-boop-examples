4bb35393e7411fc10a7b0d8ba1e4c53dca2c5eb4
James Halliday <mail@substack.net>
2013-05-14 17:58:26 -0700

    wicked drop/undrop

--- beat/beat.js
+++ beat/beat.js
@@ -4,16 +4,33 @@ var tune = [ 0, -1/6, 1/2, -3/4, -1/5, 1/6 ]
     .map(function (x) { return 800 * Math.pow(2, x) })
 ;
 var alt = [ -1/6, 4/2, 1/4, 1/3 ]
-    .map(function (x) { return 3200 * Math.pow(2, x) })
+    .map(function (x) { return 1600 * Math.pow(2, x) })
 ;
 
 var b = baudio(function (t) {
+    if (t % 32 >= 23 && t % 32 < 25) {
+        t = Math.sin((t - 23) * tau / 8); // drop
+    }
+    if (t % 32 >= 31 && t % 32 < 32) {
+        t = Math.sin((t - 26) * tau / 4); // undrop
+    }
+    if (t % 32 >= 25 && t % 32 < 31) {
+        t = 25 + (t % 32 - 25) * 0.5;
+    }
+    
     var f = 120 + Math.sin(1000 * (t % 1));
-    var n = tune[Math.floor(t * 2 % tune.length)];
+    var n = tune[Math.floor(t % tune.length)];
     
     if (t % 16 >= 15) return variant(n);
-    if (t % 32 > 17 && t % 32 < 21) {
-        var speed = t % 32 < 19 ? 4 : 8;
+    if ((t % 32 > 21 && t % 32 < 22)) {
+        return variant(n * 0.5) * sin(2); // mini-drop
+    }
+    if (t % 32 > 22 && t % 2 < 1) {
+        return (variant(n / 2) + primary(n / 2)) / 2 * sin(8); // punctuate
+    }
+    
+    if (t % 32 > 17 && t % 32 < 19) {
+        var speed = t % 32 < 18 ? 2 : 4;
         n = alt[Math.floor(t * speed % alt.length)];
         return (variant(n) + primary(n)) / 2;
     }
